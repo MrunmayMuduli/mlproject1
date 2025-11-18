32115065ee9737a2a2e3a8b0f60228dd1666493d
MRUNMAY MUDULI <mrunmaymuduli711@gmail.com>
2025-11-17 21:20:47 -0500

    Updated for debugging
    Updated for debugging

--- demo/app.py
+++ demo/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from helper import add_numbers
+from helper import add_numbers, subtract_numbers
 
 st.title("🚀 Streamlit Debug Demo")
 st.write("This is a sample Streamlit app you can debug in VS Code.")
@@ -9,4 +9,8 @@ y = st.number_input("Enter second number", value=20)
 
 if st.button("Add"):
     result = add_numbers(x, y)
+    st.success(f"Result = {result}")
+
+if st.button("Subtract"):
+    result = subtract_numbers(x, y)
     st.success(f"Result = {result}")
--- demo/helper.py
+++ demo/helper.py
@@ -1,3 +1,6 @@
 def add_numbers(a, b):
     # Put a breakpoint here!
     return a + b
+
+def subtract_numbers(a, b):
+    return a - b
